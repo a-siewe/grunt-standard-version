fdf87b2942e7ad6277047b7f006b65d82c8aa03d
Armel Siewe <asiewe@i-views.com>
2018-08-15 11:04:45 +0200

    code refactoring

--- tasks/standard_version.js
+++ tasks/standard_version.js
@@ -25,22 +25,23 @@ var DEFAULT_OPTIONS = {
   skip: {},
   dryRun: false,
   files: [],
-  push: false
+  push: true,
+  pushTo: 'upstream'
 };
 
 module.exports = function (grunt) {
 
   grunt.registerMultiTask('standard_version', 'bump your package version and generate update CHANGELOG', function () {
-    var done = this.async();
     var opt = this.options(DEFAULT_OPTIONS);
     var previousVersion = require(pkg).version;
 
     function push() {
-      var cmd = 'git push';
+      var cmd;
       if (opt.push) {
-        if (opt.dryRun) {
+      if (opt.push === 'git' && !opt.pushTo) {
+        cmd = 'git push';
+        if (dryRun) {
           grunt.log.ok(cmd);
-          return
         } else {
           exec(cmd, function (err, stdout, stderr) {
             if (err) {
@@ -51,8 +52,46 @@ module.exports = function (grunt) {
             grunt.log.ok('Pushed to the git default settings');
           });
         }
+
+        return;
       }
+
+      exec('git rev-parse --abbrev-ref HEAD', function (err, stdout, stderr) {
+        if (err) {
+          grunt.fatal('Can not get ref for HEAD:\n' + stderr);
+        }
+
+        cmd = [];
+
+        if (opt.push === true || opt.push === 'branch') {
+          cmd.push('git push ' + opt.pushTo + ' ' + stdout.trim());
+        }
+
+        if (opt.push === true || opt.push === 'tag') {
+          exec('git describe --tags', function (err, stdout, stderr) {
+            if (err) {
+              grunt.fatal('Can not get the most recent tag from git' + stderr);
+            }
+            cmd.push('git push ' + opt.pushTo + ' ' + stdout.trim());
+          });
+        }
+
+        cmd = cmd.join('&&');
+
+        if (dryRun) {
+          grunt.log.ok(cmd);
+        } else {
+          exec(cmd, function (err, stdout, stderr) {
+            if (err) {
+              grunt.fatal('Can not push to ' + opt.pushTo + ':\n  ' + stderr);
+            }
+
+            grunt.log.ok('Pushed to ' + opt.pushTo);
+          });
+        }
+      });
     }
+  }
 
 
     /**
@@ -68,8 +107,8 @@ module.exports = function (grunt) {
         var content = grunt.file.read(file).replace(VERSION_REGEXP, function (match, prefix, parsedVersion, namedPre, noNamePre, suffix) {
           return prefix + newVersion + (suffix || '');
         })
-        var logMsg = 'Version bumped to ' + newVersion +  ' (in ' + file + ')';
-        if(!opt.dryRun) {
+        var logMsg = 'Version bumped to ' + newVersion + ' (in ' + file + ')';
+        if (!opt.dryRun) {
           grunt.file.write(file, content);
         }
         grunt.log.ok(logMsg);
